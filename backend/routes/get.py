6e3a44880a57ae76eedcc1e03655627b55096295
Pham Si Anh <phamsianh97@gmail.com>
2021-09-30 17:09:12 +0200

    Update get_related_resource_collection

--- backend/routes/get.py
+++ backend/routes/get.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from routes.dependencies import oauth2_scheme as oa2s, db, user as u
 from ORM.session import Session
 from ORM import Model_description
@@ -29,11 +29,11 @@ def get_resource_instance(rsc: str, rsc_id: Union[int, str], session: Session = 
 
 
 @router.get("/{rsc}/{rsc_id}/{rel_rsc}")
-def get_related_resource_collection(rsc: str, rsc_id: Union[int, str], rel_rsc: str,
+def get_related_resource_collection(rsc: str, rsc_id: Union[int, str], rel_rsc: str, req: Request,
                                     session: Session = Depends(db.get_session), u_d: u.UserDependency = Depends()):
     helper.check_rsc_exist(rsc, Model_description.all_models.keys())
     rsc_ins = helper.get_rsc_ins(rsc, rsc_id, session, u_d)
-    rel_rscs = helper.get_rel_rsc(rsc_ins, rel_rsc)
+    rel_rscs = helper.get_rel_rsc(rsc_ins, rel_rsc, dict(req.query_params))
     if not rel_rscs:
         return None
     else:
