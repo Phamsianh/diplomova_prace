519952598df14c277466d57a7c7f2ff92bcef917
Pham Si Anh <phamsianh97@gmail.com>
2021-10-18 21:50:17 +0200

    add exceptions

---
+++ backend/exceptions/InstanceException.py
@@ -0,0 +1,26 @@
+from exceptions.ORMExceptions import ORMException
+
+
+class InstanceException(ORMException):
+    def __int__(self, message: str = None):
+        self.message = message
+        super(ORMException, self).__init__(self.message)
+
+
+class CurrentPhaseNotResolved(InstanceException):
+    def __init__(self, ins_cur_state: str):
+        self.message = f"current phase has not been resolved. current state of this instance is {ins_cur_state}"
+        super(CurrentPhaseNotResolved, self).__init__(self.message)
+
+
+class NotAvailableNextPhases(InstanceException):
+    def __init__(self, phase_id: int, ins_id: int, avl_nxt_phss: list):
+        self.message = f"instance {ins_id} can not transit to phase {phase_id}. " \
+                       f"instance can only transit to phase {', '.join(avl_nxt_phss)}"
+        super(NotAvailableNextPhases, self).__init__(self.message)
+
+
+class CurrentlyNotRequireHandle(InstanceException):
+    def __init__(self):
+        self.message = "instance currently doesn't require to be handled."
+        super(CurrentlyNotRequireHandle, self).__init__(self.message)
