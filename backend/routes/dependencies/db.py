e66f0166513352be2cb87f21ec2f4feca3704084
Pham Si Anh <phamsianh97@gmail.com>
2021-10-21 14:42:12 +0200

    create new session for each request, otherwise session will be cached

--- backend/routes/dependencies/db.py
+++ backend/routes/dependencies/db.py
@@ -1,7 +1,11 @@
-from ORM.session import session
+from ORM.engine import engine
+from sqlalchemy.orm import sessionmaker
 
 
 def get_session():
+    # we must create new session for each request, otherwise session will be cached
+    ss = sessionmaker()
+    session = ss(bind=engine, autoflush=False)
     try:
         yield session
     finally:
