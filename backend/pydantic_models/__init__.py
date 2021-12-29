ffcd06cb7edb0213b403a340f6b02a660b3ff168
Pham Si Anh <phamsianh97@gmail.com>
2021-12-29 13:39:50 +0100

    add __init__ to pydantic_models module

---
+++ backend/pydantic_models/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ['users', 'groups', 'roles', 'positions', 'users_positions',
+           'forms', 'phases', 'transitions',
+           'sections', 'fields', 'instances', 'instances_fields']
+
