fe4d7520f31b69314decf233c1923b6548a0e6ee
Pham Si Anh <phamsianh97@gmail.com>
2021-12-29 13:37:47 +0100

    add origin for react

--- backend/app_config.py
+++ backend/app_config.py
@@ -17,5 +17,6 @@ origins = [
     "http://localhost:8080",
     "http://localhost:5500",
     "http://127.0.0.1:5500",
-    "http://localhost"
+    "http://localhost",
+    "http://localhost:3000"
 ]
