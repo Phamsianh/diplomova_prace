09dfa93793a2042f5212e8ac304e21825f6f580a
Pham Si Anh <phamsianh97@gmail.com>
2022-03-28 09:18:43 +0200

    clean up backend

--- backend/app_config.py
+++ backend/app_config.py
@@ -15,11 +15,11 @@ database_name = 'test2'
 # database_name = os.environ.get('DB_NAME')
 
 # FastAPI config
-title = "Administrative work handling system"
+title = "Information System for Supporting Administration Process (ISSAP)"
 description = """
-Administrative work handling system
-is a system, which let an organization manages and operates their administrative works
-E.g: creating form, form instances, defining workflows,...
+ISSAP is information system created for supporting administration process.
+It allows user to create an administration process, define phases and transitions with a form for user to intantiate.
+ISSAP also audit all content before user transit instance to the next phase. Audit trail is used for administrative purposes.
 """
 origins = [
     "http://localhost:8080",
@@ -28,3 +28,8 @@ origins = [
     "http://localhost",
     "http://localhost:3000"
 ]
+
+# token
+SECRET_KEY = "5c303030e2c614051f0787b0a6250615247be31677e92f21afc210d2d4607c18"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 43200  #  = 30*24*60 = 1 month
